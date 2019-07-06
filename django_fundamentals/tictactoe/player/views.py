c6474f1b0d7d51a323f1ca6a9c09f120a5b076f3
doer <499361328@qq.com>
2019-07-06 09:28:44 +0800

    Django Fundamentals(7 Templates and Static Content) 4)  Demo：Passing Data from View to Template

--- django_fundamentals/tictactoe/player/views.py
+++ django_fundamentals/tictactoe/player/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
+from gameplay.models import Game
+
 
 # Create your views here.
 def home(request):
-    return render(request, "player/home.html")
+    return render(request, "player/home.html", {'ngames': Game.objects.count()})
