f4156b581b29ee2b76f2f2821ebbcb592147b2f0
doer <499361328@qq.com>
2019-07-06 09:50:27 +0800

    Django Fundamentals(7 Templates and Static Content) 5)  Demo：Displaying Game Data in a Template

--- django_fundamentals/tictactoe/player/views.py
+++ django_fundamentals/tictactoe/player/views.py
@@ -5,4 +5,9 @@ from gameplay.models import Game
 
 # Create your views here.
 def home(request):
-    return render(request, "player/home.html", {'ngames': Game.objects.count()})
+    games_first_player = Game.objects.filter(first_player=request.user, status='F')
+
+    games_second_player = Game.objects.filter(second_player=request.user, status='S')
+    all_my_games = list(games_first_player) + list(games_second_player)
+
+    return render(request, "player/home.html", {'games': all_my_games})
