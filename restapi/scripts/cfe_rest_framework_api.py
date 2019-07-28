ad401e61132283567aff6cd7aba50fa8f849218a
doer <499361328@qq.com>
2019-07-28 10:43:28 +0800

    Rest Api(3 Django Rest Framework API) 20-permission-tests-with-python-requests

--- restapi/scripts/cfe_rest_framework_api.py
+++ restapi/scripts/cfe_rest_framework_api.py
@@ -7,6 +7,23 @@ ENDPOINT = "http://127.0.0.1:8000/api/status/"
 image_path = os.path.join(os.getcwd(), "logo.jpg")
 
 
+get_endpoit = ENDPOINT + str(12)
+post_data = json.dumps({'content': "Some random content"})
+
+r = requests.get(get_endpoit)
+print(r.text)
+
+r2 = requests.get(ENDPOINT)
+print(r.status_code)
+
+post_headers = {
+    'content-type': 'application/json'
+}
+
+post_response = requests.post(ENDPOINT, data=post_data, headers=post_headers)
+print(post_response.text)
+
+
 # def do(method='get', data={}, id=1, is_json=True):
 #     if is_json:
 #         data = json.dumps(data)
@@ -14,25 +31,25 @@ image_path = os.path.join(os.getcwd(), "logo.jpg")
 #     print(r.text)
 #     return r
 
-def do_img(method='get', data={}, is_json=True, img_path=None):
-    headers = {}
-    if is_json:
-        headers["content-type"] = "application/json"
-        data = json.dumps(data)
-    if img_path is not None:
-        with open(image_path, 'rb') as image:
-            file_data = {'image': image}
-            r = requests.request(method, ENDPOINT, data=data, headers=headers, files=file_data)
-    else:
-        r = requests.request(method, ENDPOINT, data=data, headers=headers)
-
-    print(r.status_code)
-    print(r.text)
-    return r
+# def do_img(method='get', data={}, is_json=True, img_path=None):
+#     headers = {}
+#     if is_json:
+#         headers["content-type"] = "application/json"
+#         data = json.dumps(data)
+#     if img_path is not None:
+#         with open(image_path, 'rb') as image:
+#             file_data = {'image': image}
+#             r = requests.request(method, ENDPOINT, data=data, headers=headers, files=file_data)
+#     else:
+#         r = requests.request(method, ENDPOINT, data=data, headers=headers)
+#
+#     print(r.status_code)
+#     print(r.text)
+#     return r
 
 
 # do_img(method='post', data={'user': 1, 'content': ''}, is_json=False, img_path=image_path)
-do_img(method='put', data={'id': 9, 'user': 1, 'content': 'Some new content'}, is_json=False, img_path=image_path)
+# do_img(method='put', data={'id': 9, 'user': 1, 'content': 'Some new content'}, is_json=False, img_path=image_path)
 
 
 # def do(method='get', data={}, is_json=True):
