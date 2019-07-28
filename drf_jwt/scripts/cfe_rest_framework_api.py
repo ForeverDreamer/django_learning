cbbf7d3f74219b70082b7b5ecd8444229ac9efb9
doer <499361328@qq.com>
2019-07-28 16:55:43 +0800

    Rest Api(3 Django Rest Framework API) 22-jwt-authorization-header

--- drf_jwt/scripts/cfe_rest_framework_api.py
+++ drf_jwt/scripts/cfe_rest_framework_api.py
@@ -1,3 +1,4 @@
+import os
 import json
 import requests
 
@@ -5,41 +6,7 @@ import requests
 AUTH_ENDPOINT = "http://127.0.0.1:8000/api/auth/jwt/"
 REFRESH_ENDPOINT = AUTH_ENDPOINT + "refresh/"
 ENDPOINT = "http://127.0.0.1:8000/api/status/"
-
-
-def refresh_token():
-    headers = {
-        "content-type": "application/json"
-    }
-    data = {
-        'username': 'admin',
-        'password': 'password123'
-    }
-    r = requests.post(AUTH_ENDPOINT, data=json.dumps(data), headers=headers)
-    token = r.json()['token']
-
-    refresh_data = {
-        'token': token
-    }
-    new_response = requests.post(REFRESH_ENDPOINT, data=json.dumps(refresh_data), headers=headers)
-    new_token = new_response.json()
-    print(new_token)
-
-
-refresh_token()
-
-
-def get_token():
-    data = {
-        'username': 'admin',
-        'password': 'password123'
-    }
-    r = requests.post(AUTH_ENDPOINT, data=data)
-    token = r.json()['token']
-    print(token)
-
-
-# get_token()
+IMAGE_PATH = os.path.join(os.getcwd(), "logo.jpg")
 
 
 def without_token():
@@ -61,3 +28,61 @@ def without_token():
 
 
 # without_token()
+
+
+def get_token():
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = {
+        'username': 'admin',
+        'password': 'password123'
+    }
+    r = requests.post(AUTH_ENDPOINT, data=json.dumps(data), headers=headers)
+    token = r.json()['token']
+    # print(token)
+    return token
+
+
+# get_token()
+
+
+def refresh_token():
+    headers = {
+        "content-type": "application/json"
+    }
+    data = {
+        'username': 'admin',
+        'password': 'password123'
+    }
+    r = requests.post(AUTH_ENDPOINT, data=json.dumps(data), headers=headers)
+    token = r.json()['token']
+
+    refresh_data = {
+        'token': token
+    }
+    new_response = requests.post(REFRESH_ENDPOINT, data=json.dumps(refresh_data), headers=headers)
+    new_token = new_response.json()
+    print(new_token)
+
+
+# refresh_token()
+
+
+def create_status():
+    headers = {
+        # "Content-Type": "application/json",
+        "Authorization": "JWT" + ' ' + get_token(),
+    }
+
+    with open(IMAGE_PATH, 'rb') as image:
+        file_data = {
+            'image': image
+        }
+        post_data = {'content': "Some random content"}
+        print(headers)
+        post_response = requests.post(ENDPOINT, data=post_data, headers=headers, files=file_data)
+        print(post_response.text)
+
+
+create_status()
