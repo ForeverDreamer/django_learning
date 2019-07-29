2126b69ce948c15dc8c45bc784a5f799bc6ba21f
doer <499361328@qq.com>
2019-07-29 18:47:22 +0800

    Rest Api(3 Django Rest Framework API) 32-nested-serializer-part-2 33-nested-serializer-part-3

--- drf_jwt/src/drf_jwt/urls.py
+++ drf_jwt/src/drf_jwt/urls.py
@@ -20,5 +20,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/auth/', include('accounts.api.urls')),
+    url(r'^api/user/', include('accounts.api.user.urls', namespace='api-user')),
     url(r'^api/status/', include('status.api.urls')),
 ]
--- drf_jwt/src/status/api/serializers.py
+++ drf_jwt/src/status/api/serializers.py
@@ -10,6 +10,22 @@ Serializers -> validate data
 '''
 
 
+class StatusInlineUserSerializer(serializers.ModelSerializer):
+    uri = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = Status
+        fields =[
+            'uri',
+            'id',
+            'content',
+            'image'
+        ]
+
+    def get_uri(self, obj):
+        return "/api/status/{id}/".format(id=obj.id)
+
+
 class StatusSerializer(serializers.ModelSerializer):
 
     user = UserPublicSerializer(read_only=True)
@@ -42,3 +58,5 @@ class StatusSerializer(serializers.ModelSerializer):
         if content is None and image is None:
             raise serializers.ValidationError("Content or image is required.")
         return data
+
+
