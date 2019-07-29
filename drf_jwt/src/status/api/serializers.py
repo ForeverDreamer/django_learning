a8a95707aa29de3cf9e6f36b0d598ec96a4c5bdb
doer <499361328@qq.com>
2019-07-29 12:34:44 +0800

    Rest Api(3 Django Rest Framework API) 31-nested-serializer-part-1

--- drf_jwt/src/status/api/serializers.py
+++ drf_jwt/src/status/api/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from status.models import Status
+from accounts.api.serializers import UserPublicSerializer
 
 '''
 Serializers -> JSON
@@ -10,13 +11,18 @@ Serializers -> validate data
 
 
 class StatusSerializer(serializers.ModelSerializer):
+
+    user = UserPublicSerializer(read_only=True)
+    uri = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = Status
         fields = [
             'id',
             'user',
             'content',
-            'image'
+            'image',
+            'uri'
         ]
         read_only_fields = ['user']  # GET
 
@@ -24,6 +30,9 @@ class StatusSerializer(serializers.ModelSerializer):
     #     if len(value) > 10000:
     #         raise serializers.ValidationError("This is wayy too long.")
     #     return value
+
+    def get_uri(self, obj):
+        return "/api/status/{id}/".format(id=obj.id)
 
     def validate(self, data):
         content = data.get("content", None)
