193b8c3d8fcc652755a15e3aaee8e1762a377ca3
doer <499361328@qq.com>
2019-07-30 19:42:15 +0800

    Django-Angular-Ionici(1 Django Backend Setup) 4-add-bootstrap-for-api-home

--- drf_jwt/src/drf_jwt/urls.py
+++ drf_jwt/src/drf_jwt/urls.py
@@ -19,7 +19,8 @@ from django.views.generic import TemplateView
 
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='base.html'), name='home'),
+    url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
+    url(r'^api/$', TemplateView.as_view(template_name='api-home.html'), name='api-home'),
     url(r'^admin/', admin.site.urls),
     url(r'^api/auth/', include('accounts.api.urls', namespace='api-auth')),
     url(r'^api/user/', include('accounts.api.user.urls', namespace='api-user')),
