cc32d757d11036a54f64741ae2addb7e2c17521e
doer <499361328@qq.com>
2019-08-02 18:50:48 +0800

    Try Django 2.2 : 6-a-first-url-mapping 7-multiple-views 8-path-vs-re_path-vs-url

--- try_django/src/try_django/try_django/urls.py
+++ try_django/src/try_django/try_django/urls.py
@@ -14,8 +14,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path  # url
+
+from .views import (
+    home_page,
+    about_page,
+    contact_page
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_page),
+    path('page/', about_page),
+    path('pages/', about_page),
+    re_path(r'^pages?/$', about_page),
+    re_path(r'^about/$', about_page),
+    path('about/', about_page),
+    path('contact/', contact_page),
 ]
