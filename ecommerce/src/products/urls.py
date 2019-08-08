ae05a8f159bf2b34a94374bfd4afed3f1d6a136b
doer <499361328@qq.com>
2019-08-08 22:53:08 +0800

    eCommerce(3 Products Component) 11-slugfield-signals

--- ecommerce/src/products/urls.py
+++ ecommerce/src/products/urls.py
@@ -6,13 +6,15 @@ from .views import (
     ProductDetailView,
     product_detail_view,
     ProductFeaturedListView,
-    ProductFeaturedDetailView
+    ProductFeaturedDetailView,
+    ProductDetailSlugView
 )
 
 urlpatterns = [
     url(r'^$', ProductListView.as_view()),
     url(r'^fbc/$', product_list_view),
     url(r'^(?P<pk>\d+)/$', ProductDetailView.as_view()),
+    url(r'^(?P<slug>[\w-]+)/$', ProductDetailSlugView.as_view()),
     url(r'^fbc/(?P<pk>\d+)/$', product_detail_view),
     url(r'^featured/$', ProductFeaturedListView.as_view()),
     url(r'^featured/(?P<pk>\d+)/$', ProductFeaturedDetailView.as_view()),
