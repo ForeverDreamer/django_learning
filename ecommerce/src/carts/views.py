e1fd4ee867da2f4add35f8418e37266dbfc26800
doer <499361328@qq.com>
2019-08-11 17:53:53 +0800

    eCommerce(7 Cart Component) 4-create-a-cart-in-the-view

--- ecommerce/src/carts/views.py
+++ ecommerce/src/carts/views.py
@@ -1,11 +1,28 @@
 from django.shortcuts import render
 
 
+from .models import Cart
+
+
+def cart_create(user=None):
+    cart_obj = Cart.objects.create(user)
+    print("New cart created")
+    return cart_obj
+
+
 def cart_home(request):
     cart_id = request.session.get('cart_id', None)
     if cart_id is None:
-        print("Create new cart")
-        request.session['cart_id'] = 12
-    else:
+        cart_obj = cart_create()
+        request.session['cart_id'] = cart_obj.id
+        return render(request, "carts/home.html", {})
+
+    qs = Cart.objects.filter(id=cart_id)
+    if qs.count() == 1:
         print("Cart ID exists")
+        cart_obj = qs.first()
+    else:
+        cart_obj = cart_create()
+        request.session['cart_id'] = cart_obj.id
+
     return render(request, "carts/home.html", {})
