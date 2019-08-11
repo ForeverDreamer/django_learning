d6c67733d60d4e617605f5be256b81971a5fc016
doer <499361328@qq.com>
2019-08-11 11:20:24 +0800

    eCommerce(6 Search Component) 2-display-the-query-to-the-user

--- ecommerce/src/search/views.py
+++ ecommerce/src/search/views.py
@@ -4,7 +4,14 @@ from products.models import Product
 
 
 class SearchProductView(ListView):
-    template_name = "products/list.html"
+    template_name = "search/view.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(SearchProductView, self).get_context_data(**kwargs)
+        query = self.request.GET.get('q')
+        context['query'] = query
+        # SearchQuery.objects.create(query=query)
+        return context
 
     def get_queryset(self):
         """
