4eef6691bc0c5cf30585c004c4c8d0c8cdcc21b9
doer <499361328@qq.com>
2019-08-11 17:29:10 +0800

    eCommerce(7 Cart Component) 3-cart-model

--- ecommerce/src/carts/views.py
+++ ecommerce/src/carts/views.py
@@ -2,5 +2,10 @@ from django.shortcuts import render
 
 
 def cart_home(request):
-    print(dir(request.session))
-    return render(request, "carts/home.html")
+    cart_id = request.session.get('cart_id', None)
+    if cart_id is None:
+        print("Create new cart")
+        request.session['cart_id'] = 12
+    else:
+        print("Cart ID exists")
+    return render(request, "carts/home.html", {})
