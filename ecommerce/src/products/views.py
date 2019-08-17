422881fdb4ab081d5dd58abff1aacbb55576b490
doer <499361328@qq.com>
2019-08-17 12:38:11 +0800

    eCommerce Add On Sections(2 Custom Analytics) 5-object-viewed-mixin

--- ecommerce/src/products/views.py
+++ ecommerce/src/products/views.py
@@ -4,7 +4,7 @@ from django.http import Http404
 
 from .models import Product
 from carts.models import Cart
-from analytics.signals import object_viewed_signal
+from analytics.mixins import ObjectViewedMixin
 
 
 class ProductFeaturedListView(ListView):
@@ -14,7 +14,7 @@ class ProductFeaturedListView(ListView):
         return Product.objects.all().featured()
 
 
-class ProductFeaturedDetailView(DetailView):
+class ProductFeaturedDetailView(ObjectViewedMixin, DetailView):
     queryset = Product.objects.all().featured()
     template_name = "products/featured-detail.html"
 
@@ -45,7 +45,7 @@ def product_list_view(request):
     return render(request, "products/list.html", context)
 
 
-class ProductDetailSlugView(DetailView):
+class ProductDetailSlugView(ObjectViewedMixin, DetailView):
     queryset = Product.objects.all()
     template_name = "products/detail.html"
 
@@ -68,12 +68,11 @@ class ProductDetailSlugView(DetailView):
             instance = qs.first()
         except:
             raise Http404("Uhhmmm ")
-        object_viewed_signal.send(instance.__class__, instance=instance, request=self.request)
-        
+
         return instance
 
 
-class ProductDetailView(DetailView):
+class ProductDetailView(ObjectViewedMixin, DetailView):
     queryset = Product.objects.all()
     template_name = "products/detail.html"
 
