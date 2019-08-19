48056ca9e586fc3bc9ceb1e9defdbd7bdbb67d32
doer <499361328@qq.com>
2019-08-19 19:33:43 +0800

    Chat x Channels(1 WebSockets & Channels) 9-websocket-groups

--- channels/src/chat/consumers.py
+++ channels/src/chat/consumers.py
@@ -8,12 +8,21 @@ from channels.db import database_sync_to_async
 User = get_user_model()
 
 
-class ChatConsumer(AsyncConsumer):
+class ChatConsumer(AsyncConsumer):  # AsyncWebSocketConsumer
     async def websocket_connect(self, event):
         # when the socket connects
         print(event)
         print('-------------------------------')
+        
+        self.room_name = 'chatter'  # single thread id
+        self.room_group_name = f'chat_{self.room_name}'
         self.rando_user = await self.get_name()
+
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+
 
         await self.send({
             "type": "websocket.accept"
@@ -21,21 +30,45 @@ class ChatConsumer(AsyncConsumer):
 
     async def websocket_receive(self, event):  # websocket.receive
         # when the socket receive
+        # print(event)
+        # print(self.rando_user)
+        # message_data = json.loads(event['text'])
+        # print(message_data)
+        # print('-------------------------------')
+        #
+        # await self.send({
+        #     "type": "websocket.send",
+        #     "text": json.dumps(message_data)
+        # })
         print(event)
-        print(self.rando_user)
-        message_data = json.loads(event['text'])
-        print(message_data)
         print('-------------------------------')
 
+        message_data = json.loads(event['text'])
+        final_message_data = json.dumps(message_data)
+
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': final_message_data
+            }
+        )
+
+    async def chat_message(self, event):
         await self.send({
             "type": "websocket.send",
-            "text": json.dumps(message_data)
+            "text": event['message']
         })
 
     async def websocket_disconnect(self, event):
         # when the socket disconnects
         print(event)
         print('-------------------------------')
+
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
 
     @database_sync_to_async
     def get_name(self):
