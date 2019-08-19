a3dd3f9a276ff8e5f2223c580f193b301b8cbce0
doer <499361328@qq.com>
2019-08-19 12:44:15 +0800

    Chat x Channels(1 WebSockets & Channels) 2-non-realtime-app-with-django-only

--- channels/src/chat/views.py
+++ channels/src/chat/views.py
@@ -1,10 +1,10 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import Http404
-from django.shortcuts import render
-
+from django.http import Http404, HttpResponseForbidden
+from django.views.generic.edit import FormMixin
 from django.views.generic import DetailView, ListView
 
-from .models import Thread
+from .forms import ComposeForm
+from .models import Thread, ChatMessage
 
 
 class InboxView(LoginRequiredMixin, ListView):
@@ -14,8 +14,10 @@ class InboxView(LoginRequiredMixin, ListView):
         return Thread.objects.by_user(self.request.user)
 
 
-class ThreadView(LoginRequiredMixin, DetailView):
+class ThreadView(LoginRequiredMixin, FormMixin, DetailView):
     template_name = 'chat/thread.html'
+    form_class = ComposeForm
+    success_url = './'
 
     def get_queryset(self):
         return Thread.objects.by_user(self.request.user)
@@ -26,3 +28,26 @@ class ThreadView(LoginRequiredMixin, DetailView):
         if obj is None:
             raise Http404
         return obj
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = self.get_form()
+        return context
+
+    def post(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return HttpResponseForbidden()
+        self.object = self.get_object()
+        form = self.get_form()
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+
+    def form_valid(self, form):
+        thread = self.get_object()
+        user = self.request.user
+        message = form.cleaned_data.get('message')
+        ChatMessage.objects.create(user=user, thread=thread, message=message)
+
+        return super().form_valid(form)
