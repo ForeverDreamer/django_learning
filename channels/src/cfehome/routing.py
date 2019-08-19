2bf7b0a26136ee3763300b3346f3780d02a31534
doer <499361328@qq.com>
2019-08-19 16:42:36 +0800

    Chat x Channels(1 WebSockets & Channels) 4-websocket-routing

--- channels/src/cfehome/routing.py
+++ channels/src/cfehome/routing.py
@@ -1,5 +1,17 @@
-from channels.routing import ProtocolTypeRouter
+from django.conf.urls import url
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
 
 application = ProtocolTypeRouter({
-
+    # Websocket chat handler
+    'websocket': AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(
+                [
+                    # url(r"chat/", ChatConsumer, name='chat')
+                ]
+            )
+        ),
+    )
 })
