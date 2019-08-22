d94056162f91684df1a032076f95e2e8850db38e
doer <499361328@qq.com>
2019-08-22 15:52:33 +0800

    Srvup 2: 10-customize-the-admin

--- srvup/src/videos/admin.py
+++ srvup/src/videos/admin.py
@@ -2,4 +2,15 @@ from django.contrib import admin
 
 from .models import Video
 
-admin.site.register(Video)
+
+class VideoAdmin(admin.ModelAdmin):
+    list_filter = ['timestamp', 'updated']
+    list_display = ['title', 'timestamp', 'updated']
+    readonly_fields = ['timestamp', 'updated', 'short_title']
+    search_fields = ['title', 'embed_code']
+
+    class Meta:
+        model = Video
+
+
+admin.site.register(Video, VideoAdmin)
--- srvup/src/videos/models.py
+++ srvup/src/videos/models.py
@@ -4,7 +4,11 @@ from django.db import models
 class Video(models.Model):
     title = models.CharField(max_length=120)
     embed_code = models.TextField()
-    timestamp = models.DateTimeField(auto_now_add=True)
+    timestamp = models.DateTimeField(auto_now_add=True)  # time created
+    updated = models.DateTimeField(auto_now=True)  # last saved
+
+    def short_title(self):
+        return self.title[:3]
 
     def __str__(self):
         return self.title
