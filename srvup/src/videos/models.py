30e09882801059d2226754f6786b9eabd6a4eee3
doer <499361328@qq.com>
2019-08-23 12:27:54 +0800

    Srvup 2: 18-slugfield-slug-lookups

--- srvup/src/videos/models.py
+++ srvup/src/videos/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Video(models.Model):
     title = models.CharField(max_length=120)
+    slug = models.SlugField(blank=True)
     embed_code = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)  # time created
     updated = models.DateTimeField(auto_now=True)  # last saved
--- srvup/src/videos/urls.py
+++ srvup/src/videos/urls.py
@@ -4,5 +4,6 @@ from .views import VideoListView, VideoDetailView
 
 urlpatterns = [
     path('', VideoListView.as_view(), name='video-list'),
-    path('<str:pk>/', VideoDetailView.as_view(), name='video-detail'),
+    # path('<str:pk>/', VideoDetailView.as_view(), name='video-detail'),
+    path('<str:slug>/', VideoDetailView.as_view(), name='video-detail'),
 ]
