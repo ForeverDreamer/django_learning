ec22af8b96b49a5e13bf032bf7f574809dc96a12
doer <499361328@qq.com>
2019-08-23 12:16:39 +0800

    Srvup 2: 17-video-detail-view

--- srvup/src/videos/urls.py
+++ srvup/src/videos/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import VideoListView
+from .views import VideoListView, VideoDetailView
 
 urlpatterns = [
     path('', VideoListView.as_view(), name='video-list'),
+    path('<str:pk>/', VideoDetailView.as_view(), name='video-detail'),
 ]
--- srvup/src/videos/views.py
+++ srvup/src/videos/views.py
@@ -28,6 +28,11 @@ class VideoCreateView(CreateView):
 class VideoDetailView(DetailView):
     queryset = Video.objects.all()
 
+    def get_context_data(self, *args, **kwargs):
+        context = super(VideoDetailView, self).get_context_data(*args, **kwargs)
+        print(context)
+        return context
+
 
 class VideoUpdateView(UpdateView):
     queryset = Video.objects.all()
