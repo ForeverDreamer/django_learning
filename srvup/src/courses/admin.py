5656ba91178f9e2dc785d691ed09e0e3ab547c60
doer <499361328@qq.com>
2019-08-26 12:25:03 +0800

    Srvup 2: 36-add-lecture-to-admin

--- srvup/src/courses/admin.py
+++ srvup/src/courses/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
 
-from .models import Course
+from .models import Course, Lecture
 
 
-admin.site.register(Course)
+class LectureInline(admin.TabularInline):
+    model = Lecture
+
+
+class CourseAdmin(admin.ModelAdmin):
+    inlines = [LectureInline]
+    list_filter = ['updated', 'timestamp']
+    list_display = ['title', 'updated', 'timestamp']
+    readonly_fields = ['updated', 'timestamp', 'short_title']
+    search_fields = ['title', 'description']
+
+    class Meta:
+        model = Course
+
+    def short_title(self, obj):
+        return obj.title[:3]
+
+
+admin.site.register(Course, CourseAdmin)
