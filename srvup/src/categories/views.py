2ab39f4e5195274822d390ff0a3cc52498943e64
doer <499361328@qq.com>
2019-08-27 19:11:23 +0800

    Srvup 2: 48-category-list-groups

--- srvup/src/categories/views.py
+++ srvup/src/categories/views.py
@@ -7,7 +7,7 @@ from .models import Category
 
 
 class CategoryListView(ListView):
-    queryset = Category.objects.all()
+    queryset = Category.objects.all().order_by('title')
 
 
 class CategoryDetailView(DetailView):
